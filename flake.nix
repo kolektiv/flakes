8e4220158b7c06a7ed6e13b1242b4277a6464e23
Andrew Cherry <andrew@xyncro.com>
2024-02-10 19:48:43 +0000

    updating default, etc.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,23 @@
 
   outputs = {self}: {
     templates = {
+      default = self.rust;
+
       node = {
         path = ./node;
         description = "Node Development Environment";
       };
+
       rust = {
-        path = ./rust;
         description = "Rust Development Environment";
+        path = ./rust;
+        welcomeText = ''
+          # Rust Development Template
+
+          Intended as a quick starting point for new Rust projects. It is
+          expected that the flake will be expanded to include any other needed
+          dependencies, tools, etc.
+        '';
       };
     };
   };
