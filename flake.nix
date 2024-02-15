00d95464bae852e6ec2506752a1db1d432f810a1
Andrew Cherry <andrew@xyncro.com>
2024-02-15 15:41:22 +0000

    quick elixir flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,18 @@
     templates = {
       default = self.rust;
 
+      elixir = {
+        description = "Elixir Development Environment";
+        path = ./elixir;
+        welcomeText = ''
+          # Elixir Development Template
+
+          Intended as a quick starting point for new Elixir projects. It is
+          expected that the flake will be expanded to include any other needed
+          dependencies, tools, etc.
+        '';
+      };
+
       node = {
         path = ./node;
         description = "Node Development Environment";
